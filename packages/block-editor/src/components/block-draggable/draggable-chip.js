895654f44fdf1c52d9732d0a5b93739f860ccac1
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2025-01-01 10:10:52 +0530

    Add JS Doc

--- packages/block-editor/src/components/block-draggable/draggable-chip.js
+++ packages/block-editor/src/components/block-draggable/draggable-chip.js
@@ -10,6 +10,32 @@ import { dragHandle } from '@wordpress/icons';
  */
 import BlockIcon from '../block-icon';
 
+/**
+ * The BlockDraggableChip component renders a chip that can display
+ * additional metadata for draggable blocks.
+ *
+ * @example
+ * ```jsx
+ * function Example() {
+ *   return (
+ *     <BlockDraggableChip
+ *       count={3}
+ *       icon={<svg>...</svg>}
+ *       isPattern={true}
+ *       fadeWhenDisabled={false}
+ *     />
+ *   );
+ * }
+ * ```
+ *
+ * @param {Object}        props                  The props for the BlockDraggableChip component.
+ * @param {number}        props.count            The number of blocks associated with the draggable chip.
+ * @param {string|Object} props.icon             The icon of the block. This can be any of [WordPress' Dashicons](https://developer.wordpress.org/resource/dashicons/), or a custom `svg` element.
+ * @param {boolean}       props.isPattern        Whether the chip represents a pattern block.
+ * @param {boolean}       props.fadeWhenDisabled Whether to visually fade the chip when dragging is disabled.
+ *
+ * @return {Element} The rendered BlockDraggableChip component.
+ */
 export default function BlockDraggableChip( {
 	count,
 	icon,
